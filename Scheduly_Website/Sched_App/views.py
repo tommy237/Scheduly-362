c9ffdf4d2eee6356cd23d82fe18c05f1e6a026ca
tommy237 <tommytran237@csu.fullerton.edu>
2025-03-05 13:28:41 -0800

    re-organized website urls

--- Scheduly_Website/Sched_App/views.py
+++ Scheduly_Website/Sched_App/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def home_page(request):
-    template=loader.get_template('home.html')
+def makePage(name):
+    template=loader.get_template(name)
     return HttpResponse(template.render())
+
+def home_page(request):
+    return makePage('home.html')
 
 def about_page(request):
-    template=loader.get_template('about.html')
-    return HttpResponse(template.render())
+    return makePage('about.html')
 
 def login_page(request):
-    template=loader.get_template('login.html')
-    return HttpResponse(template.render())
+    return makePage('login.html')
 
 # Create your views here.
