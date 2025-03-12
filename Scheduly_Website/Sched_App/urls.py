9b648443ef5e69c52aae8f813e8dd8e874ab06b6
tommy237 <tommytran237@csu.fullerton.edu>
2025-03-12 09:24:13 -0700

    added javascript file to inegrate interactions

--- Scheduly_Website/Sched_App/urls.py
+++ Scheduly_Website/Sched_App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.about_page, name='About [] Scheduly'),
-     path('home/', views.home_page, name='Scheduly'),
-      path('login/', views.login_page, name='Login [] Scheduly'),
+    path('about/', views.about_page, name='about-page'),
+     path('home/', views.home_page, name='home-page'),
+      path('login/', views.login_page, name='login-page'),
 ]
