5d56f3cac68a416bd8bb0069ae907dc698a6aab0
stephanyyyyy <stephany0614@csu.fullerton.edu>
2025-03-24 18:24:09 -0700

    fixing home page, urls.py and views.py

--- Scheduly_Website/Sched_App/urls.py
+++ Scheduly_Website/Sched_App/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+#from django.http import HttpResponse
 
 urlpatterns = [
+    path('', views.home, name='home-page'), 
     path('about/', views.about_page, name='about-page'),
-     path('home/', views.home_page, name='home-page'),
+     path('home/', views.home, name='home-page'),
       path('login/', views.login_page, name='login-page'),
 ]
