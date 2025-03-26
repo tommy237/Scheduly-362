e7b3a1a676d4830f1ef62844ef627615867f061d
tommy237 <tommytran237@csu.fullerton.edu>
2025-03-26 11:38:22 -0700

    hahahahahahahahahahahahahahahahahahahhahahahahahahahahahahahahhahahahahahahaha modified login page

---
+++ Scheduly_Website/Sched_App/static/home.js
@@ -0,0 +1,28 @@
+import "./home.css"
+
+const loadr=document.querySelector<HTMLDivElement>(".LOAD_CONT");
+window.onload=()=>{
+    setTimeout(function(){
+        loadr.style.opacity="0";
+        setTimeout(function(){
+            loadr.style.display="none";
+        },500);
+    },5000);
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    let cX,cY,tgX,tgY=0;
+    const bub=document.querySelector<HTMLDivElement>(".GRAD.I");
+    function move(){
+        cX+=(tgX-cX)/20;
+        cY+=(tgY-cY)/20;
+        bub.style.transform=`translate(${Math.round(cX)}px, ${Math.round(cY)}px)`;
+        requestAnimationFrame(()=>{move();});
+    }
+    
+    window.addEventListener('mousemove',(event)=>{
+        tgX=event.clientX;
+        tgY=event.clientY;});
+    move();
+});
+
