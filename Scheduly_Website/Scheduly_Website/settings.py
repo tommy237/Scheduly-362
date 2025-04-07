0f6b747cf5de0b92d78376e63ad619c81e25bbfd
stephanyyyyy <stephany0614@csu.fullerton.edu>
2025-04-07 14:18:22 -0700

    changes to settings.py

--- Scheduly_Website/Scheduly_Website/settings.py
+++ Scheduly_Website/Scheduly_Website/settings.py
@@ -82,22 +82,15 @@ WSGI_APPLICATION = 'Scheduly_Website.wsgi.application'
 #}
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scheduly_database',
+        'USER': 'root',
+        'PASSWORD': '1234pass',
+        'HOST': 'localhost',   # or IP/domain if remote
+        'PORT': '3306',        # default MySQL port
+    }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'scheduly_database',
-#         'USER': 'root',
-#         'PASSWORD': '1234pass',
-#         'HOST': 'localhost',   # or IP/domain if remote
-#         'PORT': '3306',        # default MySQL port
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
