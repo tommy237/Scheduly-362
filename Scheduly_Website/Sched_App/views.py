643c9f2ebfeb84fe61d4a7876b11923e0851034c
stephanyyyyy <stephany0614@csu.fullerton.edu>
2025-04-08 16:31:01 -0700

    subtle change for views

--- Scheduly_Website/Sched_App/views.py
+++ Scheduly_Website/Sched_App/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages 
 from django.contrib.auth import authenticate, login, logout
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # for database
 from django.shortcuts import render, redirect
 from .forms import CustomUserForm
